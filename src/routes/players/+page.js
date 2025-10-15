ee528aa17c251e6c1a3e7f8ee3e399616a7d0cb8
mintsj1 <mintsj1@student.op.ac.nz>
2025-10-15 15:53:56 +1300

    Created fetch for players

--- src/routes/players/+page.js
+++ src/routes/players/+page.js
@@ -1,4 +1,5 @@
 export async function load({ fetch }) {
     const response = await fetch('https://my.api.mockaroo.com/players.json?key=1c2be0a0')
     const players = await response.json();
+    return { players };
 }
