12dd795ca1c0cd21cf613e0169d582768477ea59
mintsj1 <mintsj1@student.op.ac.nz>
2025-10-15 15:54:10 +1300

    created loop to show player name and email

---
+++ src/routes/players/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    let { data } = $props();
+    console.log(data);
+</script>
+
+<h1>Players</h1>
+<ul>
+    {#each data.players as player}
+        <li>{player.first_name} - {player.email}</li>
+    {/each}
+</ul>
