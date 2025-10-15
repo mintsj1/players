681bc37c51ec28304a720c72f041ea45eb6dce62
mintsj1 <mintsj1@student.op.ac.nz>
2025-10-15 16:00:16 +1300

    set up dynamic route for page per player

--- src/routes/players/+page.svelte
+++ src/routes/players/+page.svelte
@@ -6,6 +6,6 @@
 <h1>Players</h1>
 <ul>
     {#each data.players as player}
-        <li>{player.first_name} - {player.email}</li>
+        <li><a href="/players/{player.id}">{player.first_name} {player.last_name} - {player.email}</a></li>
     {/each}
 </ul>
